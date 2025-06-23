31853ff07ee78b1685b0df40d6fc038ede871b53
Mir Sabiq <mirsabiq04@gmail.com>
2025-06-23 21:22:27 +0530

    New Code

--- test.c
+++ test.c
@@ -1,6 +1,14 @@
+// C program named mainreturn.c to demonstrate the working
+// of command line arguement
 #include <stdio.h>
-int main()
+
+// defining main with arguments
+int main(int argc, char* argv[])
 {
-    printf("hello");
-return 0;    
+    printf("You have entered %d arguments:\n", argc);
+
+    for (int i = 0; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
+    return 0;
 }
